db26cb1482a7e6481d9354921f0353c5c735915b
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2023-09-19 23:23:34 +0900

    refactor: 웹훅 테스트5

--- src/main/java/com/docker/app/main/controller/MainController.java
+++ src/main/java/com/docker/app/main/controller/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
 
     @GetMapping("/jenkins-app")
     public ResponseEntity<?> jenkins() {
-        return new ResponseEntity<>("------- Jenkins Build Test -------", HttpStatus.OK);
+        return new ResponseEntity<>("------- Jenkins Build -------", HttpStatus.OK);
     }
 
     @GetMapping("/github-webhook")
